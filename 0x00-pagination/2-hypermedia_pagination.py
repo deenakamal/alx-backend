df7e652460fc068f8d88c385d973f4f753abf54c
deenakamal <deenah.kamal@gmail.com>
2024-07-22 06:20:52 +0000

    fix

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -45,7 +45,7 @@ class Server:
         total_pages = math.ceil(total_items / page_size)
 
         return {
-            'page_size': len(data),  # Length of the data returned
+            'page_size': len(data),
             'page': page,
             'data': data,
             'next_page': page + 1 if page < total_pages else None,
