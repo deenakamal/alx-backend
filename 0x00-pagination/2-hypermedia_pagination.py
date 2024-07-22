8e1b4e7ce748021211c6a6d7b6e695294b37d7b8
deenakamal <deenah.kamal@gmail.com>
2024-07-22 06:04:02 +0000

    task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Module"""
+import csv
+import math
+from typing import List, Dict
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ func returns start and end index """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page rows from the dataset"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = self.index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """ Returns a dictionary containing the following key-value"""
+        start_idx, end_idx = index_range(page, page_size)
+        dataset_len = len(self.dataset())
+
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if end_idx < dataset_len else None,
+            'prev_page': page - 1 if start_idx > 0 else None,
+            'total_pages': math.ceil(dataset_len / page_size)
+        }
