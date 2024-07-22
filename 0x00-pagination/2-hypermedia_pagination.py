b3fc1886d612107753a2d438243ddd4fffca00d4
deenakamal <deenah.kamal@gmail.com>
2024-07-22 06:25:33 +0000

    22

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-"""Module"""
+"""Defines Server class."""
+
 import csv
 import math
 from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ func returns start and end index """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    """
+    Calculate the start and end indices for a given page and page size
+    in a paginated list.
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+
     return (start_index, end_index)
 
 
@@ -32,14 +37,20 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get page rows from the dataset"""
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
-        start, end = self.index_range(page, page_size)
-        return self.dataset()[start:end]
+        """Retrieves a page of data"""
+        assert (type(page) == int and page > 0)
+        assert (type(page_size) == int and page_size > 0)
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Returns a dictionary containing the following key-value pairs"""
+        """Retrieves info about a page"""
         start_idx, end_idx = index_range(page, page_size)
         dataset_len = len(self.dataset())
 
