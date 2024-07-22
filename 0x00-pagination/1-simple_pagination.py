141c55718cfc28ab89fdedebe218024451be0bab
deenakamal <deenah.kamal@gmail.com>
2024-07-22 05:58:45 +0000

    edit

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,7 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-         """Get page rows from the dataset"""
+        """Get page rows from the dataset"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         start, end = self.index_range(page, page_size)
