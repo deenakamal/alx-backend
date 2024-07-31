5f380206d0a3738231245124c0844d76d0f09774
deenakamal <deenah.kamal@gmail.com>
2024-07-31 01:37:24 +0000

    4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,7 +21,7 @@ def get_locale():
     """Get the locale Languages"""
     param = request.args.get("locale")
     if param in app.config["LANGUAGES"]:
-        return locale_param
+        return param
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
